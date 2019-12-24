ecc482b67d020de1166bb9f9fef5accb91e310de
Mukundan Senthil <mukundan314@gmail.com>
2019-12-24 10:28:42 +0530

    Add paste model

--- pasteapp/models.py
+++ pasteapp/models.py
@@ -1,3 +1,10 @@
+import uuid
 from django.db import models
 
-# Create your models here.
+
+class Paste(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    title = models.CharField(max_length=64)
+    language = models.CharField(max_length=32)
+    content = models.TextField()
+    created_on = models.DateField(auto_now_add=1)
