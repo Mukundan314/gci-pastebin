b8c9dfef19fe2efe572a9c76604c309b80b74fdf
Mukundan Senthil <mukundan314@gmail.com>
2019-12-25 22:09:27 +0530

    Return 404 if paste does not exist

--- pasteapp/views.py
+++ pasteapp/views.py
@@ -1,6 +1,6 @@
 import json
 
-from django.http import JsonResponse
+from django.http import JsonResponse, Http404
 from django.shortcuts import render
 from django.views.decorators.http import require_POST
 from django.views.decorators.csrf import csrf_exempt
@@ -40,11 +40,14 @@ def create(request):
 
 
 def view(request, paste_id):
-    paste = Paste.objects.get(id=paste_id)
+    try:
+        paste = Paste.objects.get(id=paste_id)
 
-    return render(request, 'view.html', {
-        'title': paste.title,
-        'content': paste.content,
-        'language': paste.language,
-        'created_on': paste.created_on,
-    })
+        return render(request, 'view.html', {
+            'title': paste.title,
+            'content': paste.content,
+            'language': paste.language,
+            'created_on': paste.created_on,
+        })
+    except:
+        raise Http404('Paste does not exist')
